8a5d660f9eecee1f8926d068bb5f2ebf6cccbb39
Matt Holcombe (gullinbursti) <github@gullinbursti-creations.com>
2012-09-06 00:56:17 -0700

    first commit

---
+++ ios/HotOrNot/HotOrNot/HONAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HONAppDelegate.h
+//  HotOrNot
+//
+//  Created by Matthew Holcombe on 09.05.12.
+//  Copyright (c) 2012 Built in Menlo, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HONViewController;
+
+@interface HONAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) HONViewController *viewController;
+
+@end
