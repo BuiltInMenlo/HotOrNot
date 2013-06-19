c92222dde2f623c0759e48fcbbc43b84e5b57fda
Matt Holcombe (gullinbursti) <github@gullinbursti-creations.com>
2013-06-19 14:18:53 -0700

    normalizing camera experience

--- ios/HotOrNot/HotOrNot/Classes/HONAvatarCameraOverlayView.h
+++ ios/HotOrNot/HotOrNot/Classes/HONAvatarCameraOverlayView.h
@@ -2,27 +2,12 @@
 //  HONAvatarCameraOverlayView.h
 //  HotOrNot
 //
-//  Created by Matthew Holcombe on 04.30.13.
+//  Created by Matthew Holcombe on 06.19.13 @ 09:23 AM.
 //  Copyright (c) 2013 Built in Menlo, LLC. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@protocol HONAvatarCameraOverlayViewDelegate;
 @interface HONAvatarCameraOverlayView : UIView
-- (void)showPreviewNormal:(UIImage *)image;
-- (void)showPreviewFlipped:(UIImage *)image;
-- (void)hidePreview;
 
-@property(nonatomic, assign) id <HONAvatarCameraOverlayViewDelegate> delegate;
-@end
-
-@protocol HONAvatarCameraOverlayViewDelegate
-- (void)cameraOverlayViewTakePicture:(HONAvatarCameraOverlayView *)cameraOverlayView;
-- (void)cameraOverlayViewCancelCamera:(HONAvatarCameraOverlayView *)cameraOverlayView;
-- (void)cameraOverlayViewSubmit:(HONAvatarCameraOverlayView *)cameraOverlayView;
-@optional
-- (void)cameraOverlayViewChangeFlash:(HONAvatarCameraOverlayView *)cameraOverlayView;
-- (void)cameraOverlayViewChangeCamera:(HONAvatarCameraOverlayView *)cameraOverlayView;
-- (void)cameraOverlayViewShowCameraRoll:(HONAvatarCameraOverlayView *)cameraOverlayView;
 @end
