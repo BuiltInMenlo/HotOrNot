7c2b3a6662016e8c5fed16994683ec094896db0f
Shane Hill <shanehill00@gmail.com>
2013-06-22 14:58:13 +0000

    fixing tracking functionality to be more reusable. also made some fixes the controller layer to allow us to have a more modern api if we so choose

--- php/api-shane/classes/BIM/DAO/Mysql/Persona.php
+++ php/api-shane/classes/BIM/DAO/Mysql/Persona.php
@@ -2,20 +2,15 @@
 
 class BIM_DAO_Mysql_Persona extends BIM_DAO_Mysql{
 	public function getData( $name ){
-		$sql = "
-			select * 
-			from growth.persona
-			where name = ?
-		";
+		$sql = "select * from growth.persona where name = ?";
 		$params = array( $name );
 		$stmt = $this->prepareAndExecute( $sql, $params );
-		
 		return $stmt->fetchAll( PDO::FETCH_CLASS, 'stdClass' );
 	}
 	
 	public function trackInboundClick( $name, $networkId, $referer = '' ){
-	    $sql = "insert into growth.inbound_persona_clicks (name, networkId, referer) values (?, ?, ?)";
-		$params = array( $name, $networkId, $referer );
+	    $sql = "insert into growth.inbound_persona_clicks (name, network_id, referer, time) values (?, ?, ?,?)";
+		$params = array( $name, $networkId, $referer, time() );
 		$stmt = $this->prepareAndExecute( $sql, $params );
 	}
 }
