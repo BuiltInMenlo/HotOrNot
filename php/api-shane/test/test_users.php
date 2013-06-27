c4acaa6a97253b861b5016578f9a77cf124a470e
Shane Hill <shanehill00@gmail.com>
2013-06-27 21:46:43 +0000

    improvement to a test script

--- php/api-shane/test/test_users.php
+++ php/api-shane/test/test_users.php
@@ -1,52 +1,56 @@
 <?php
 require_once 'vendor/autoload.php';
 
-/*
-$params = (object) array(
-    'user_id' => 881,
-    'hashed_number' => 'hash999',
-    'hashed_list' => array('hash666','hash9','hash3','hash665'),
-);
-*/
+testFriendmatching();
 
-$params = (object) array(
-    'id' => 882,
-    'hashed_number' => '+14152549393',
-    'hashed_list' => array('hash666_7','hash9_7','+14152549391','hash665_7'),
-);
+function testFriendmatching(){
+    /*
+    $params = (object) array(
+        'user_id' => 881,
+        'hashed_number' => 'hash999',
+        'hashed_list' => array('hash666','hash9','hash3','hash665'),
+    );
+    */
+    
+    $params = (object) array(
+        'id' => 882,
+        'hashed_number' => '+14152549393',
+        'hashed_list' => array('hash666_7','hash9_7','+14152549391','hash665_7'),
+    );
+    
+    $users = new BIM_App_Users();
+    $friends = $users->matchFriends( $params );
+    
+    print_r( json_encode( $friends ) );
+}
 
-$users = new BIM_App_Users();
-$friends = $users->matchFriends( $params );
-
-print_r( json_encode( $friends ) );
-
-/*
-$id = '881';
-
-print_r( $smsCode = BIM_Utils::getSMSCodeForId( $id ) );
-print_r("\n");
-print_r( BIM_Utils::getIdForSMSCode( $smsCode ) );
-print_r("\n");
-
-$params = array(
-    'AccountSid' => 'ACb76dc4d9482a77306bc7170a47f2ea47',
-    'Body' => "$smsCode",
-    'ToZip' => '34109',
-    'FromState' => 'CA',
-    'ToCity' => 'NAPLES',
-    'SmsSid' => 'SM0014f9ec1d891dfca69d2d3a7eee43d2',
-    'ToState' => 'FL',
-    'To' => '+12394313268',
-    'ToCountry' => 'US',
-    'FromCountry' => 'US',
-    'SmsMessageSid' => 'SM0014f9ec1d891dfca69d2d3a7eee43d2',
-    'ApiVersion' => '2010-04-01',
-    'FromCity' => 'SAN FRANCISCO',
-    'SmsStatus' => 'received',
-    'From' => '+14152549392',
-    'FromZip' => '94930',
-);
-
-$users = new BIM_App_Users();
-$users->linkMobileNumber( (object) $params );
-*/
+function testTwilioCallback(){
+    $id = '881';
+    
+    print_r( $smsCode = BIM_Utils::getSMSCodeForId( $id ) );
+    print_r("\n");
+    print_r( BIM_Utils::getIdForSMSCode( $smsCode ) );
+    print_r("\n");
+    
+    $params = array(
+        'AccountSid' => 'ACb76dc4d9482a77306bc7170a47f2ea47',
+        'Body' => "$smsCode",
+        'ToZip' => '34109',
+        'FromState' => 'CA',
+        'ToCity' => 'NAPLES',
+        'SmsSid' => 'SM0014f9ec1d891dfca69d2d3a7eee43d2',
+        'ToState' => 'FL',
+        'To' => '+12394313268',
+        'ToCountry' => 'US',
+        'FromCountry' => 'US',
+        'SmsMessageSid' => 'SM0014f9ec1d891dfca69d2d3a7eee43d2',
+        'ApiVersion' => '2010-04-01',
+        'FromCity' => 'SAN FRANCISCO',
+        'SmsStatus' => 'received',
+        'From' => '+14152549392',
+        'FromZip' => '94930',
+    );
+    
+    $users = new BIM_App_Users();
+    $users->linkMobileNumber( (object) $params );
+}
