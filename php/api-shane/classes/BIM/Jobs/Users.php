a8b30a2451cccbb1fd5c5f67067f565018856381
Shane Hill <shanehill00@gmail.com>
2013-06-28 21:26:37 +0000

    moving the twilio friend matching and push notification jobs to the queue

---
+++ php/api-shane/classes/BIM/Jobs/Users.php
@@ -0,0 +1,30 @@
+<?php 
+
+class BIM_Jobs_Users extends BIM_Jobs{
+    
+    /**
+     * 
+     * @param int|string $userId - volley user id
+     * @param array $addresses - list of email addresses, pipe delimited
+     */
+    public static function queueFindfFriends( $list ){
+        $job = array(
+        	'class' => 'BIM_Jobs_Users',
+        	'method' => 'findFriends',
+        	'data' => $list,
+        );
+        
+        return self::queueBackground( $job, 'find_friends' );
+    }
+	
+    public static function findFriends( $workload ){
+        // now we perform a search and send out push notification
+        $list = $workload->data;
+        $users = new BIM_App_Users;
+        $matches = $users->findfriends($list);
+        $j = new BIM_Jobs_Growth();
+	    foreach( $matches as $match ){
+	        $j->queueMatchPush( $match, $list );
+	    }
+    }
+}
