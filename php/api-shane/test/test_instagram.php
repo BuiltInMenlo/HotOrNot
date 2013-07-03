8358e1de3a1d30c5ef1f0960a7952cb48509fd91
Shane Hill <shanehill00@gmail.com>
2013-07-03 04:21:18 +0000

    changing profile links in instagram

--- php/api-shane/test/test_instagram.php
+++ php/api-shane/test/test_instagram.php
@@ -2,6 +2,8 @@
 require_once 'vendor/autoload.php';
 
 $persona = (object) array(
+    'name' => 'shanehill00',
+    'type' => 'volley',
     'instagram' => (object) array(
     	'email' => 'shanehill00@gmail.com',
     	'username' => 'shanehill00',
@@ -17,7 +19,7 @@ $persona = (object) array(
 );
 
 // leyla - 37438644
-$user = (object) array( 'id' => 37438644 );
+//$user = (object) array( 'id' => 37438644 );
 
 // shane - 37459491
 //$user = (object) array( 'id' => 37459491 );
@@ -27,11 +29,12 @@ $persona = new BIM_Growth_Persona( $persona );
 $routines = new BIM_Growth_Instagram_Routines( $persona );
 
 // fed2208ec99f11e2af6f22000a1f9a09_7
-$media = (object) array( 'id' => 'fed2208ec99f11e2af6f22000a1f9a09_7' );
+//$media = (object) array( 'id' => 'fed2208ec99f11e2af6f22000a1f9a09_7' );
 
 try{
-    $routines->loginAndAuthorizeApp();
-    print_r( $routines->volleyUserPhotoComment() );
+    $link = 'http://getvolleyapp.com/ooh/ooh56';
+    $routines->dropLinkInBio($link);
+//    print_r( $routines->volleyUserPhotoComment() );
 //    print_r( json_decode( $routines->comment( "nice one", $media ) ) );
 } catch( Exception $e ){
     print_r( $e );
