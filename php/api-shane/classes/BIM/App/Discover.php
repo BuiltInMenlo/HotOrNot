68588de43cc70c9054d60fea9e9be2563239cb5d
Shane Hill <shanehill00@gmail.com>
2013-08-01 04:49:25 +0000

    finished refactoring the discover class to support 1 to many volleys

--- php/api-shane/classes/BIM/App/Discover.php
+++ php/api-shane/classes/BIM/App/Discover.php
@@ -13,118 +13,9 @@ class BIM_App_Discover extends BIM_App_Base{
 	 * @param $challenge_id The ID of the challenge to get (integer)
 	 * @return An associative object for a challenge (array)
 	**/
-	public function getChallengeObj ($challenge_id) {
-		$this->dbConnect();
-	    $challenge_arr = array();
-		
-		// get challenge row
-		$query = 'SELECT * FROM `tblChallenges` WHERE `id` = '. $challenge_id .';';
-		$challenge_obj = mysql_fetch_object(mysql_query($query));
-					
-		// get subject title for this challenge
-		$query = 'SELECT `title` FROM `tblChallengeSubjects` WHERE `id` = '. $challenge_obj->subject_id .';';
-		$subject_obj = mysql_fetch_object(mysql_query($query));
-		
-		// get total number of comments
-		$query = 'SELECT `id` FROM `tblComments` WHERE `challenge_id` = '. $challenge_id .';';
-		$comments = mysql_num_rows(mysql_query($query));
-		
-		$expires = -1;
-        if( !empty( $challenge_obj->expires ) && $challenge_obj->expires > -1 ){
-            $expires = $challenge_obj->expires - time();
-            if( $expires < 0 ){
-                $expires = 0;
-            }
-        }
-		
-        // compose object
-		$challenge_arr = array(
-			'id' => $challenge_obj->id, 
-			'status' => $challenge_obj->status_id, 
-			'subject' => $subject_obj->title, 
-			'comments' => $comments, 
-			'has_viewed' => $challenge_obj->hasPreviewed, 
-			'started' => $challenge_obj->started, 
-			'added' => $challenge_obj->added, 
-			'creator' => $this->userForChallenge($challenge_obj->creator_id, $challenge_obj->id),
-			'challenger' => $this->userForChallenge($challenge_obj->challenger_id, $challenge_obj->id),
-		    'expires' => $expires
-		); 
-		
-		// return
-		return ($challenge_arr);
+	public function getChallengeObj ($volleyId) {
+	    return new BIM_Model_Volley($volleyId);
 	}
-	
-	/**
-	 * Helper function to user info for a challenge
-	 * @param $user_id The creator or challenger ID (integer)
-	 * @param $challenge_id The challenge's ID to get the user for (integer)
-	 * @return An associative object for a user (array)
-	**/
-	public function userForChallenge($user_id, $challenge_id) {
-		$this->dbConnect();
-	    
-		// prime the user
-		$user_arr = array(
-			'id' => $user_id, 
-			'fb_id' => "",
-			'username' => "",
-			'avatar' => "", 
-			'img' => "",
-			'score' => 0				
-		);
-		
-		// challenge object
-		$query = 'SELECT `status_id`, `creator_id`, `challenger_id`, `creator_img`, `challenger_img` FROM `tblChallenges` WHERE `id` = '. $challenge_id .';';
-		$challenge_obj = mysql_fetch_object(mysql_query($query));
-		
-		// user is the creator
-		if ($user_id == $challenge_obj->creator_id) {
-			$query = 'SELECT `fb_id`, `username`, `img_url` FROM `tblUsers` WHERE `id` = '. $user_id .';';
-			$user_arr['img'] = $challenge_obj->creator_img;
-						
-		// user is the challenger
-		} else {
-			$query = 'SELECT `fb_id`, `username`, `img_url` FROM `tblUsers` WHERE `id` = '. $user_id .';';
-			$user_arr['img'] = $challenge_obj->challenger_img;			
-			
-			// invited challenger
-			if ($challenge_obj->status_id == "7")
-				$query = 'SELECT `fb_id`, `username` FROM `tblInvitedUsers` WHERE `id` = '. $user_id .';';
-		}
-		
-		// user object
-		$user_obj = mysql_fetch_object(mysql_query($query));
-		
-		// votes for challenger
-		$query = 'SELECT `challenger_id` FROM `tblChallengeVotes` WHERE `challenge_id` = '. $challenge_id .';';
-	   	$score_result = mysql_query($query);
-					
-		while ($score_row = mysql_fetch_assoc($score_result)) {										
-			if ($score_row['challenger_id'] == $user_id)
-				$user_arr['score']++;
-		}
-		
-		// user info
-		if ($user_obj != null) {
-			$user_arr['fb_id'] = $user_obj->fb_id;
-			$user_arr['username'] = $user_obj->username;
-			
-			// find the avatar image
-			if ($user_obj->img_url == "") {
-				if ($user_obj->fb_id == "")
-					$user_arr['avatar'] = "https://s3.amazonaws.com/hotornot-avatars/defaultAvatar.png";
-				
-				else
-					$user_arr['avatar'] = "https://graph.facebook.com/". $user_obj->fb_id ."/picture?type=square";
-		
-			} else
-				$user_arr['avatar'] = $user_obj->img_url;
-		}
-		
-		return ($user_arr);
-	}
-	
 	
 	/**
 	 * Gets the challenges by total votes
@@ -149,7 +40,7 @@ class BIM_App_Discover extends BIM_App_Base{
 					
 			// push challenge into array
 			$co = $this->getChallengeObj( $row['id'] );
-			if( $co['expires'] != 0 ){
+			if( $co->expires != 0 ){
     			array_push( $challenge_arr, $co );
 			}
 		}
