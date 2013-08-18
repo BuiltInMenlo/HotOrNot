eb5276e9dd6a75b762b18d49bc4b9caeb5285d73
Shane Hill <shanehill00@gmail.com>
2013-08-18 02:08:23 +0000

    fixing the age check on erver side

--- classes/BIM/Utils.php
+++ classes/BIM/Utils.php
@@ -156,7 +156,7 @@ class BIM_Utils{
 	    $OK = false;
         $birthdate = new DateTime( $birthdate );
         $cutoffBirthdate = new DateTime();
-        $cutoffBirthdate->sub( new DateInterval('P19Y') );
+        $cutoffBirthdate->sub( new DateInterval('P20Y') );
         if( $cutoffBirthdate < $birthdate ){
             $OK = true;
         }
