ab9b4771dc030d5b2647cac12ef003b48a791801
Shane Hill <shanehill00@gmail.com>
2013-08-19 04:32:56 +0000

    adding a property to indicate if the use is suspended

--- classes/BIM/Model/Volley.php
+++ classes/BIM/Model/Volley.php
@@ -63,7 +63,16 @@ class BIM_Model_Volley{
             $this->expires = $volley->expires;
             $this->is_private = $volley->is_private;
             $this->is_verify = $volley->is_verify;
+            $this->is_suspended = $this->isSuspended();
         }
+    }
+    
+    public function isSuspended(){
+        $suspended = false;
+        if( !empty( $this->abuse_ct ) && $this->abuse_ct >= 10 ){
+            $suspended = true;
+        }
+        return $suspended;
     }
     
     public function comment( $userId, $text ){
