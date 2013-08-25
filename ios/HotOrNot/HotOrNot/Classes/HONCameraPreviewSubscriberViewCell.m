b9461ac64242ee9b19e1943f386d8aa19311688e
Matt Holcombe <github@gullinbursti-creations.com>
2013-08-24 22:32:46 -0700

    new camera / first run / subscribers

---
+++ ios/HotOrNot/HotOrNot/Classes/HONCameraPreviewSubscriberViewCell.m
@@ -0,0 +1,29 @@
+//
+//  HONSendSubscriberViewCell.m
+//  HotOrNot
+//
+//  Created by Matt Holcombe on 8/24/13.
+//  Copyright (c) 2013 Built in Menlo, LLC. All rights reserved.
+//
+
+#import "HONSendSubscriberViewCell.h"
+
+@implementation HONSendSubscriberViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
