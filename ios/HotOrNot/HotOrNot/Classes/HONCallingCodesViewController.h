276a809ff148684ff7f3fcded7242bc51d8ab203
Matt Holcombe <github@gullinbursti-creations.com>
2014-05-03 17:29:06 -0700

    add view controller for first run calling code picker

---
+++ ios/HotOrNot/HotOrNot/Classes/HONCallingCodesViewController.h
@@ -0,0 +1,13 @@
+//
+//  HONCallingCodesViewController.h
+//  HotOrNot
+//
+//  Created by Matt Holcombe on 05/02/2014 @ 10:08 .
+//  Copyright (c) 2014 Built in Menlo, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HONCallingCodesViewController : UIViewController
+
+@end
