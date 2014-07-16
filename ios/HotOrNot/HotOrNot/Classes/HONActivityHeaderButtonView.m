cac510b9110753901c362b609629af829b76f30b
Anirudh Agarwala <Anirudh@Anirudhs-MacBook-Air.local>
2014-07-16 11:59:07 -0700

    Activity number removed

--- ios/HotOrNot/HotOrNot/Classes/HONActivityHeaderButtonView.m
+++ ios/HotOrNot/HotOrNot/Classes/HONActivityHeaderButtonView.m
@@ -35,14 +35,14 @@ const CGFloat kMaxActivityWidth = 44.0;
 		[_activityBGImageView setCenter:kOrthodoxActivityCenterPt];
 		_activityBGImageView.hidden = YES;
 		[self addSubview:_activityBGImageView];
-		
+		/*
 		_activityTotalLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0, 0.0, kMaxActivityWidth, 44.0)];
 		_activityTotalLabel.font = [[[HONFontAllocator sharedInstance] helveticaNeueFontRegular] fontWithSize:15];
 		_activityTotalLabel.textColor = [UIColor whiteColor];
 		_activityTotalLabel.backgroundColor = [UIColor clearColor];
 		_activityTotalLabel.textAlignment = NSTextAlignmentCenter;
 		[_activityBGImageView addSubview:_activityTotalLabel];
-		
+		*/
 		[self updateActivityBadge];
 	}
 	
