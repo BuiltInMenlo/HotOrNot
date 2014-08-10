2c6b8ae3ba99a5b4b6917b9609ab15517ffb0a46
Ojas Goyal <ojas@builtinmenlo.com>
2014-08-09 18:17:36 -0700

    Localized

--- ios/HotOrNot/HotOrNot/Classes/HONActivityItemVO.m
+++ ios/HotOrNot/HotOrNot/Classes/HONActivityItemVO.m
@@ -32,10 +32,10 @@
 		vo.message = [vo.username stringByAppendingString:@" verified your selfie"];
 	
 	} else if (vo.activityType == HONActivityItemTypeInviteRequest) {
-		vo.message = [vo.username stringByAppendingString:[NSString stringWithFormat:@" sent you an invite to %@", vo.clubName]];
+		vo.message = [vo.username stringByAppendingString:[NSString stringWithFormat: NSLocalizedString(@"sent_invite", nil), vo.clubName]]; //@" sent you an invite to %@", vo.clubName]];
 	
 	} else if (vo.activityType == HONActivityItemTypeInviteAccepted) {
-		vo.message = [vo.username stringByAppendingString:[NSString stringWithFormat:@" accepted your invite to %@", vo.clubName]];
+		vo.message = [vo.username stringByAppendingString:[NSString stringWithFormat: NSLocalizedString(@"accept_invite", nil), vo.clubName ]];//  @" accepted your invite to %@", vo.clubName]];
 	
 	} else if (vo.activityType == HONActivityItemTypeLike) {
 		vo.message = [vo.username stringByAppendingString:[NSString stringWithFormat: NSLocalizedString(@"liked_selfie", nil), vo.clubName]];
