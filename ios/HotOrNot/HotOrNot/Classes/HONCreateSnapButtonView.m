7dd1ffeee5b0f01fcc98761ad7b5c97e82c13a98
edsnyder <eric@Erics-MacBook-Pro.local>
2014-09-05 13:25:04 -0700

    asdf

--- ios/HotOrNot/HotOrNot/Classes/HONCreateSnapButtonView.m
+++ ios/HotOrNot/HotOrNot/Classes/HONCreateSnapButtonView.m
@@ -17,8 +17,8 @@
 	if ((self = [super initWithFrame:CGRectMake(227.0, 1.0, 93.0, 44.0)])) {
 		UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
 		button.frame = CGRectMake(0.0, 0.0, 93.0, 44.0);
-		[button setBackgroundImage:[UIImage imageNamed:(isLightStyle) ? @"headerCameraWhiteButton_nonActive" : @"headerCameraButton_nonActive"] forState:UIControlStateNormal];
-		[button setBackgroundImage:[UIImage imageNamed:(isLightStyle) ? @"headerCameraWhiteButton_Active" : @"headerCameraButton_Active"] forState:UIControlStateHighlighted];
+		[button setBackgroundImage:[UIImage imageNamed:(isLightStyle) ? @"cameraButton_nonActive" : @"cameraButton_nonActive"] forState:UIControlStateNormal];
+		[button setBackgroundImage:[UIImage imageNamed:(isLightStyle) ? @"cameraButton_Active" : @"cameraButton_Active"] forState:UIControlStateHighlighted];
 		[button addTarget:target action:action forControlEvents:UIControlEventTouchDown];
 		[self addSubview:button];
 	}
